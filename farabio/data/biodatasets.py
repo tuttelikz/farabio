9b449faf80a95102caf2a54bd26bd6a2245457d9
San Askaruly <s.askaruly@gmail.com>
2021-04-07 04:40:31 +0900

    adding RetinopathyDataset

--- farabio/data/biodatasets.py
+++ farabio/data/biodatasets.py
@@ -1,7 +1,12 @@
 import os
 import subprocess
+from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
+from zipfile import ZipFile
+import pandas as pd
+from PIL import Image
+import torch
 
 
 kaggle_biodatasets = [
@@ -53,6 +58,43 @@ def download_datasets(tag, path = "."):
     print(output)
 
     os.chdir(prev_cwd)
+
+def extract_zip(fzip, fnew):
+    with ZipFile(fzip, 'r') as zip: 
+        print('Extracting all the train files now...') 
+        zip.extractall(fnew)
+        print('Done!') 
+
+
+class RetinopathyDataset(Dataset):
+    """RetinopathyDataset from https://www.kaggle.com/c/aptos2019-blindness-detection/overview
+    """
+    def __init__(self, root: str, train: bool = True, download: bool = True):
+        tag = "aptos2019-blindness-detection"
+
+        if download:
+            download_datasets(tag, path=root)
+
+        extract_zip(os.path.join(root, tag+".zip"),os.path.join(root, tag))
+
+        if train:
+            self.csv_path = os.path.join(root, tag, "train.csv")
+            self.img_path = os.path.join(root, tag, "train_images")
+
+        self.data = pd.read_csv(self.csv_path)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.img_path, self.data.loc[idx, 'id_code'] + '.png')
+        image = Image.open(img_name)
+        image = image.resize((256, 256), resample=Image.BILINEAR)
+        label = torch.tensor(self.data.loc[idx, 'diagnosis'])
+        return {   
+            'image': transforms.ToTensor()(image),
+            'labels': label
+        }
 
 
 class ChestXray(ImageFolder):
