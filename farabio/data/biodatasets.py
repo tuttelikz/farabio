1bca0d58c19e858f78d79c98b7843f70df500396
San Askaruly <s.askaruly@gmail.com>
2021-04-07 02:34:31 +0900

    kaggle datasets download function

--- farabio/data/biodatasets.py
+++ farabio/data/biodatasets.py
@@ -1,6 +1,58 @@
 import os
+import subprocess
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
+
+
+kaggle_biodatasets = [
+    "aptos2019-blindness-detection",
+    "chest-xray-pneumonia",
+    "data-science-bowl-2018",
+    "histopathologic-cancer-detection",
+    "intel-mobileodt-cervical-cancer-screening",
+    "skin-cancer-mnist"
+]
+
+def download_datasets(tag, path = "."):
+    """[summary]
+
+    Parameters
+    ----------
+    tag : str
+        tag for dataset
+
+        .. note::
+            available tags:
+            kaggle_biodatasets = [
+                "aptos2019-blindness-detection",
+                "chest-xray-pneumonia",
+                "data-science-bowl-2018",
+                "histopathologic-cancer-detection",
+                "intel-mobileodt-cervical-cancer-screening",
+                "skin-cancer-mnist"
+            ]
+    path : str, optional
+        path where to save dataset, by default "."
+    Examples
+    ----------
+    >>> download_datasets(tag="skin-cancer-mnist", path=".")
+    """
+    if tag == "chest-xray-pneumonia":
+        bash_c_tag = ["kaggle", "datasets", "download", "-d", "paultimothymooney/chest-xray-pneumonia"]
+    elif tag == "skin-cancer-mnist":
+        bash_c_tag = ["kaggle", "datasets", "download", "-d", "kmader/skin-cancer-mnist-ham10000"]
+    else:
+        bash_c = ["kaggle", "competitions", "download", "-c"]
+        bash_c_tag = bash_c.copy()
+        bash_c_tag.append(tag)
+    
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    process = subprocess.Popen(bash_c_tag, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output)
+
+    os.chdir(prev_cwd)
 
 
 class ChestXray(ImageFolder):
