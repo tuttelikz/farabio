aa059ce32f6fa1ac3b14b2926dc69285e602922e
San Askaruly <s.askaruly@gmail.com>
2021-08-01 13:13:21 +0900

    adding weight init in resnet

--- farabio/models/classification/conv/resnet.py
+++ farabio/models/classification/conv/resnet.py
@@ -10,7 +10,8 @@ import torch.nn as nn
 from typing import Union, List, Any, Type, Optional
 from farabio.utils.helpers import get_num_parameters
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
+__all__ = ['ResNet', 'resnet18', 'resnet34',
+           'resnet50', 'resnet101', 'resnet152']
 
 cfgs = {
     'resnet18': [2, 2, 2, 2],
@@ -34,6 +35,7 @@ def conv1x1(
         bias=False
     )
 
+
 def conv3x3(
     in_planes: int,
     out_planes: int,
@@ -52,8 +54,7 @@ def conv3x3(
         dilation=dilation
     )
 
-            
-# Figure 5 of paper
+
 class BasicBlock(nn.Module):
     expansion: int = 1
 
@@ -65,7 +66,7 @@ class BasicBlock(nn.Module):
         downsample: Optional[nn.Module] = None
     ) -> None:
         super(BasicBlock, self).__init__()
-              
+
         self.conv1 = conv3x3(in_planes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
@@ -73,28 +74,29 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.stride = stride
         self.downsample = downsample
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         identity = x
-        
+
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-        
+
         out = self.conv2(out)
         out = self.bn2(out)
-        
+
         if self.downsample is not None:
             identity = self.downsample(x)
 
         out += identity
         out = self.relu(out)
-        
+
         return out
 
-        
+
 class Bottleneck(nn.Module):
     expansion: int = 4
+
     def __init__(
         self,
         in_planes: int,
@@ -112,27 +114,27 @@ class Bottleneck(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.stride = stride
         self.downsample = downsample
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         identity = x
 
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-        
+
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
-        
+
         out = self.conv3(out)
         out = self.bn3(out)
-        
+
         if self.downsample is not None:
             identity = self.downsample(x)
-        
+
         out += identity
         out = self.relu(out)
-        
+
         return out
 
 
@@ -145,18 +147,25 @@ class ResNet(nn.Module):
     ) -> None:
         super(ResNet, self).__init__()
         self.in_planes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7,
+                               stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, n_classes)
-        
+
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
         
     def _make_layer(
         self,
@@ -170,11 +179,11 @@ class ResNet(nn.Module):
             downsample = nn.Sequential(
                 conv1x1(self.in_planes, planes * block.expansion, stride),
                 nn.BatchNorm2d(planes * block.expansion)
-        )
-        
+            )
+
         layers = []
-        layers.append(block(self.in_planes,planes, stride, downsample))
-        
+        layers.append(block(self.in_planes, planes, stride, downsample))
+
         self.in_planes = planes * block.expansion
         for _ in range(1, num_blocks):
             layers.append(block(self.in_planes, planes))
@@ -186,19 +195,20 @@ class ResNet(nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
-        
+
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        
+
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
 
         x = self.fc(x)
-        
+
         return x
-    
+
+
 def _resnet(
     block: Type[Union[BasicBlock, Bottleneck]],
     layers: List[int],
@@ -207,25 +217,31 @@ def _resnet(
     model = ResNet(block, layers, **kwargs)
     return model
 
+
 def resnet18(**kwargs: Any) -> ResNet:
     return _resnet(BasicBlock, cfgs['resnet18'], **kwargs)
+
 
 def resnet34(**kwargs: Any) -> ResNet:
     return _resnet(BasicBlock, cfgs['resnet34'], **kwargs)
 
+
 def resnet50(**kwargs: Any) -> ResNet:
     return _resnet(Bottleneck, cfgs['resnet50'], **kwargs)
+
 
 def resnet101(**kwargs: Any) -> ResNet:
     return _resnet(Bottleneck, cfgs['resnet101'], **kwargs)
 
+
 def resnet152(**kwargs: Any) -> ResNet:
     return _resnet(Bottleneck, cfgs['resnet152'], **kwargs)
 
+
 def test(convnet="resnet18"):
-    x = torch.randn(1,3,224,224)
-    
-    tests = { 
+    x = torch.randn(1, 3, 224, 224)
+
+    tests = {
         "resnet18": resnet18(),
         "resnet34": resnet34(),
         "resnet50": resnet50(),
@@ -235,8 +251,8 @@ def test(convnet="resnet18"):
 
     model = tests[convnet]
     y = model(x)
-    
+
     print("Trainable parameters: ", get_num_parameters(model, True))
     print("in shape: ", x.shape, ", out shape: ", y.shape)
-    
-#test("resnet152")
+
+# test("resnet152")
--- farabio/models/classification/conv/vgg.py
+++ farabio/models/classification/conv/vgg.py
@@ -27,37 +27,38 @@ class VGG(nn.Module):
     def __init__(self, convnet: nn.Module, n_classes: int = 1000) -> None:
         super(VGG, self).__init__()
         self.conv = convnet
-        self.avgpool = nn.AdaptiveAvgPool2d((7,7))
-        
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout()
-        
+
         l1 = self._fc(nn.Linear(512*7*7, 4096))
         l2 = self._fc(nn.Linear(4096, 4096))
-        
+
         self.fc = nn.Sequential(l1, l2)
         self.final = nn.Linear(4096, n_classes)
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
         x = self.final(x)
-        
+
         return x
-    
+
     def _fc(self, linear):
         return nn.Sequential(
             linear,
             self.relu,
             self.dropout
         )
-    
+
+
 def make_layers(cfg: List[Union[str, int]], batch_norm: bool = False) -> nn.Sequential:
     layers: List[nn.Module] = []
     in_channels = 3
-    
+
     for v in cfg:
         if v == "M":
             v = cast(str, v)
@@ -65,48 +66,58 @@ def make_layers(cfg: List[Union[str, int]], batch_norm: bool = False) -> nn.Sequ
         else:
             v = cast(int, v)
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-        
+
             if batch_norm:
                 layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
             else:
                 layers += [conv2d, nn.ReLU(inplace=True)]
-            
+
             in_channels = v
-    
+
     return nn.Sequential(*layers)
 
+
 def _vgg(cfg: str, batch_norm: bool, **kwargs) -> VGG:
-    model = VGG(convnet = make_layers(cfg, batch_norm=batch_norm), **kwargs)
+    model = VGG(convnet=make_layers(cfg, batch_norm=batch_norm), **kwargs)
     return model
+
 
 def vgg11(**kwargs) -> VGG:
     return _vgg(cfgs['A'], False, **kwargs)
 
+
 def vgg11_bn(**kwargs) -> VGG:
     return _vgg(cfgs['A'], True, **kwargs)
+
 
 def vgg13(**kwargs) -> VGG:
     return _vgg(cfgs['B'], False, **kwargs)
 
+
 def vgg13_bn(**kwargs) -> VGG:
     return _vgg(cfgs['B'], True, **kwargs)
+
 
 def vgg16(**kwargs) -> VGG:
     return _vgg(cfgs['D'], False, **kwargs)
 
+
 def vgg16_bn(**kwargs) -> VGG:
     return _vgg(cfgs['D'], True, **kwargs)
+
 
 def vgg19(**kwargs) -> VGG:
     return _vgg(cfgs['E'], False, **kwargs)
 
+
 def vgg19_bn(**kwargs) -> VGG:
     return _vgg(cfgs['E'], True, **kwargs)
 
+
 def test(convnet="vgg11"):
-    x = torch.randn(1,3,224,224)
-    
-    tests = { 
+    x = torch.randn(1, 3, 224, 224)
+
+    tests = {
         "vgg11": vgg11(),
         "vgg11_bn": vgg11_bn(),
         "vgg13": vgg13(),
@@ -116,10 +127,10 @@ def test(convnet="vgg11"):
         "vgg19": vgg19(),
         "vgg19_bn": vgg19_bn(n_classes=3)
     }
-    
+
     model = tests[convnet]
     y = model(x)
-    
+
     print("Trainable parameters: ", get_num_parameters(model))
     print("in shape: ", x.shape, ", out shape: ", y.shape)
 
