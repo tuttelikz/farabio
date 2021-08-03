fc95ac6d11351e6f19022d26303a27fda6906678
San Askaruly <s.askaruly@gmail.com>
2021-08-03 22:21:33 +0900

    deleting unused classification models

--- farabio/models/classification/conv/__init__.py
+++ farabio/models/classification/conv/__init__.py
@@ -6,16 +6,3 @@ from .squeezenet import *
 from .shufflenetv2 import *
 from .mobilenetv2 import *
 from .mobilenetv3 import *
-from .dpn import *
-from .lenet import *
-from .senet import *
-from .pnasnet import *
-from .densenet import *
-from .shufflenet import *
-from .resnext import *
-from .preact_resnet import *
-from .mobilenet import *
-from .efficientnet import *
-from .regnet import *
-from .dla_simple import *
-from .dla import *
