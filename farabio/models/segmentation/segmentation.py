f1d2b4b05795cdd9062a1d01c928c736f79770b6
San Askaruly <s.askaruly@gmail.com>
2021-08-11 17:28:17 +0900

    adding deeplab with resnet backbone

--- farabio/models/segmentation/segmentation.py
+++ farabio/models/segmentation/segmentation.py
@@ -3,6 +3,7 @@ from torchvision import transforms
 from farabio.models._utils import IntermediateLayerGetter
 from farabio.models.classification.conv import resnet
 from farabio.models.segmentation.fcn.fcn import FCN, FCNHead
+from farabio.models.segmentation.deeplab.deeplabv3 import DeepLabHead, DeepLabV3
 from farabio.utils.helpers import get_num_parameters
 
 __all__ = ['fcn_resnet50', 'fcn_resnet101']
@@ -25,7 +26,7 @@ def _segm_model(name, backbone_name, num_classes, aux, pretrained_backbone=True)
     aux_classifier = None
 
     model_map = {
-        #'deeplabv3': (DeepLabHead, DeepLabV3),
+        'deeplabv3': (DeepLabHead, DeepLabV3),
         'fcn': (FCNHead, FCN)
     }
     classifier = model_map[name][0](out_inplanes, num_classes)
@@ -48,13 +49,21 @@ def fcn_resfcn_resnet101(n_classes=10, aux_loss=None, **kwargs):
     return _load_model('fcn', 'resnet101', n_classes, aux_loss, **kwargs)
 
 
+def deeplabv3_resnet50(num_classes=21, aux_loss=None, **kwargs):
+    return _load_model('deeplabv3', 'resnet50', num_classes, aux_loss, **kwargs)
+
+
+def deeplabv3_resnet101(num_classes=21, aux_loss=None, **kwargs):
+    return _load_model('deeplabv3', 'resnet101', num_classes, aux_loss, **kwargs)
+
+
 def test():
-    x = torch.Tensor(1, 3, 256, 256)
+    x = torch.Tensor(4, 3, 256, 256)
     preprocess = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     input_tensor = preprocess(x)
 
-    model = fcn_resnet50()
+    model = deeplabv3_resnet101()
     y = model(input_tensor)
 
     print("Trainable parameters: ", get_num_parameters(model))
